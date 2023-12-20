03fbd95644c09acf8045c5b736f4d91a020a4b6a
jabir254 <jabirkullow@gmail.com>
2023-12-20 12:33:32 +0300

    added view option

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const customerController = require("../controllers/customerController");
 
-
 //customer routes
 
 router.get("/", customerController.homepage);
 
 router.get("/add", customerController.addCustomer);
 router.post("/add", customerController.postCustomer);
+
+router.get("/view/:id", customerController.view);
 
 module.exports = router;
