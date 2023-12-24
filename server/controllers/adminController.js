f92c01ed8ff0810905ee13abf19de6781201188e
jabir254 <jabirkullow@gmail.com>
2023-12-24 20:41:17 +0300

    admin bro controller

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,6 +1,15 @@
+const AdminBro = require('admin-bro');
+const AdminBroExpress = require('@admin-bro/express');
 const Admin = require('../models/Admin');
 const mongoose = require('mongoose');
 
+
+const adminBro = new AdminBro({
+    databases: [],
+    rootPath: '/admin',
+});
+
+const router = AdminBroExpress.buildRouter(adminBro);
 /**
  * Post
  * admin register
