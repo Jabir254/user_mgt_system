4ecd1efc35807dcb92715dc464bb78e0bfe6839c
jabir254 <jabirkullow@gmail.com>
2024-01-09 19:48:26 +0300

    added post to signin

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const adminController = require("../controllers/adminController");
 
 router.post("/register", adminController.addAdmin);
-router.get('/dashboard', adminController.postDashboard);
+router.post('/dashboard', adminController.postDashboard);
 router.get("/signin", adminController.loginAdmin);
 
 module.exports = router;
