52126659b28fca0976fe21c9d53a7e77729678be
jabir254 <jabirkullow@gmail.com>
2024-01-09 23:08:31 +0300

    added verification

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
-app.get("/user", Verify, (req, res) => {
+import { Verify, VerifyRole } from "../middleware/verify.js";
+
+app.get("/v1/admin", Verify, VerifyRole, (req, res) => {
   res.status(200).json({
     status: "success",
-    message: "Welcome to the your Dashboard!",
+    message: "Welcome to the Admin portal!",
   });
 });
