d13abccfc8dd6fc0d88a9e2ea8b74b28b50d7dbf
jabir254 <jabirkullow@gmail.com>
2024-01-09 19:54:11 +0300

    added bcrypt to user input

--- server/models/Admin.js
+++ server/models/Admin.js
@@ -1,22 +1,44 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const adminSchema = new mongoose.Schema({
-    firstName:{
-        type:String,
-        required: true
+const adminSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
     },
-    lastName:{
-        type:String,
-        required: true
+    lastName: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required: true
+    email: {
+      type: String,
+      required: true,
     },
-    password:{
-        type:String,
-        required: true
-    }
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "Admin",
+    },
+  },
+  { timestamps: true }
+);
+UserSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return next();
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      user.password = hash;
+      next();
+    });
+  });
 });
-
-module.exports = mongoose.model('admin', adminSchema);
+module.exports = mongoose.model("admin", adminSchema);
