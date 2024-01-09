44c83a5e4564caba380c2d78829571e02707d710
jabir254 <jabirkullow@gmail.com>
2024-01-09 20:04:42 +0300

    admin registration logic

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -3,52 +3,45 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 
 /**
- * create a new admin
- * @param {new Admin} req
- * @param {body} res
+ * @route POST v1/auth/register
+ * @desc Registers a user
+ * @access Public
  */
-exports.addAdmin = async (req, res) => {
-  console.log(req.body);
-
-  const newAdmin = new Admin({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password,
-  });
-
+export async function Register(req, res) {
+  // get required variables from request body
+  // using es6 object destructing
+  const { firstName, lastName, email, password } = req.body;
   try {
-    await Admin.create(newAdmin);
-    await req.flash("info", "New admin has been added");
-
-    res.redirect("/dashboard");
-  } catch (error) {
-    console.log(error);
+      // create an instance of a user
+      const newUser = new User({
+          firstName,
+          lastName,
+          email,
+          password,
+      });
+      // Check if user already exists
+      const existingUser = await User.findOne({ email });
+      if (existingUser)
+          return res.status(400).json({
+              status: "failed",
+              data: [],
+              message: "It seems you already have an account, please log in instead.",
+          });
+      const savedUser = await newUser.save(); // save new user into the database
+      const { role, ...user_data } = savedUser._doc;
+      res.status(200).json({
+          status: "success",
+          data: [user_data],
+          message:
+              "Thank you for registering with us. Your account has been successfully created.",
+      });
+  } catch (err) {
+      res.status(500).json({
+          status: "error",
+          code: 500,
+          data: [],
+          message: "Internal Server Error",
+      });
   }
-};
-
-/**
- * login in with the admin creditials
- */
-
-exports.loginAdmin = async (req, res) => {
-  const { email, password } = req.body;
-  if (email == email && password == password) {
-    const token = jwt.sign({ password }, "your-secret-key", {
-      expiresIn: "1h",
-    });
-    res.render("admin/signin");
-  } else {
-    res.status(401).json({ error: "Invalid username or password" });
-  }
-};
-/**
- * render dashboard
- */
-exports.postDashboard = async (req, res) => {
-  const locals = {
-    title: "user management system",
-    description: "landing page",
-  };
-  res.render("admin/dashboard");
-};
+  res.end();
+}
