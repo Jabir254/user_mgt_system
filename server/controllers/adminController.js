b1ae4f5b1ad475c2560ccf8ddc22c7b70eecdd7a
jabir254 <jabirkullow@gmail.com>
2024-01-09 21:41:17 +0300

    login controller

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,6 +1,7 @@
 const Admin = require("../models/Admin");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 exports.Register = async function (req, res) {
   // get required variables from request body
@@ -41,6 +42,52 @@ exports.Register = async function (req, res) {
       message: "Internal Server Error",
     });
     console.log(err);
+  }
+  res.end();
+};
+
+exports.Login = async function (req, res) {
+  // Get variables for the login process
+  const { email } = req.body;
+  try {
+    // Check if user exists
+    const user = await User.findOne({ email }).select("+password");
+    if (!user)
+      return res.status(401).json({
+        status: "failed",
+        data: [],
+        message:
+          "Invalid email or password. Please try again with the correct credentials.",
+      });
+    // if user exists
+    // validate password
+    const isPasswordValid = bcrypt.compare(
+      `${req.body.password}`,
+      user.password
+    );
+    // if not valid, return unathorized response
+    if (!isPasswordValid)
+      return res.status(401).json({
+        status: "failed",
+        data: [],
+        message:
+          "Invalid email or password. Please try again with the correct credentials.",
+      });
+    // return user info except password
+    const { password, ...user_data } = user._doc;
+
+    res.status(200).json({
+      status: "success",
+      data: [user_data],
+      message: "You have successfully logged in.",
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      code: 500,
+      data: [],
+      message: "Internal Server Error",
+    });
   }
   res.end();
 };
