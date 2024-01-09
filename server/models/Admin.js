0ab56501ea38df736042d4c73274085bab033007
jabir254 <jabirkullow@gmail.com>
2024-01-09 22:27:13 +0300

    added jwt Token

--- server/models/Admin.js
+++ server/models/Admin.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jsonwebtoken = require("jsonwebtoken");
-const dotenv = require("dotenv").config()
+const dotenv = require("dotenv").config();
 
 dotenv.config();
 
 const { jwtSecret } = process.env;
-
-
 
 const adminSchema = new mongoose.Schema(
   {
@@ -27,8 +25,8 @@ const adminSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
-      select:false,
-      max: 25
+      select: false,
+      max: 25,
     },
     role: {
       type: String,
@@ -52,4 +50,13 @@ adminSchema.pre("save", function (next) {
     });
   });
 });
+
+adminSchema.methods.generateAccessJWT = function () {
+  let payload = {
+    id: this._id,
+  };
+  return jwt.sign(payload, jwtSecret, {
+    expiresIn: "20m",
+  });
+};
 module.exports = mongoose.model("admin", adminSchema);
